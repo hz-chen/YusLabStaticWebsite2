710e978a4fa072fada4a7ac16080b764f321e9bb
Hongzhou Chen <hongzhoc@amazon.com>
2021-01-21 15:55:49 -0800

    upload button worked, S3 access denied

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const AWS = require('aws-sdk');
+
 const rawFileUploadS3UploadBucket = 'yu-lab-raw-file-upload';
 const bucketRegion = 'us-west-2';
 const IdentityPoolId = 'us-west-2:f4276906-7876-496b-b1b0-e6d22577128d';
@@ -14,11 +16,13 @@ const s3 = new AWS.S3({
   params: {Bucket: rawFileUploadS3UploadBucket}
 });
 
+document.getElementById("uploadButton").addEventListener("click", uploadFile, false);
+
 function uploadFile() {
 
   console.log("uploadFile invoked");
 
-  let file = document.getElementById("uploadedFile").files[0];
+  let file = document.getElementById("uploadedFileDiv").files[0];
   let formData = new FormData();
 
   formData.append("file", file);
@@ -30,22 +34,17 @@ function uploadFile() {
   console.log("FileKey: " + fileKey);
 
   // Use S3 ManagedUpload class as it supports multipart uploads
-  const upload = new AWS.S3.ManagedUpload({
-    params: {
-      Bucket: rawFileUploadS3UploadBucket,
-      Key: fileKey,
-      Body: file,
-    }
+  const params = {
+    Body: file,
+    Bucket: rawFileUploadS3UploadBucket,
+    Key: fileKey,
+    ServerSideEncryption: "AES256",
+    Tagging: "key1=value1&key2=value2"
+  };
+
+  s3.putObject(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
   });
 
-  const promise = upload.promise();
-
-  promise.then(
-    function(data) {
-      console.log("Successfully uploaded file.");
-    },
-    function(err) {
-      console.log("There was an error uploading your file: ", err.message);
-    }
-  );
 }
